34b6b816d456e636e61479d1feb3fadf8beb930d
Jan Zípek <jan@zipek.cz>
2025-02-18 22:00:09 +0100

    Improved skinning

--- skin_shop_item.gd
+++ skin_shop_item.gd
@@ -6,6 +6,7 @@ signal skin_pressed(skin_data: SkinData)
 @export var name_label: Label
 @export var cost_label: Label
 @export var icon_texture: TextureRect
+@export var selected_style: StyleBox
 
 var skin_data: SkinData
 
@@ -17,3 +18,10 @@ func set_data(data: SkinData, owned: bool, _selected: bool):
     icon_texture.texture = data.icon
     name_label.text = data.name
     cost_label.text = str(data.cost) if !owned else "Owned"
+
+    if _selected:
+        add_theme_stylebox_override("normal", selected_style)
+        add_theme_stylebox_override("hover", selected_style)
+    else:
+        remove_theme_stylebox_override("normal")
+        remove_theme_stylebox_override("hover")
