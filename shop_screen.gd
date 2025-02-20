2bb1065d4ea3ecd56db30ed4fe1bd3c8328073fe
Jan Zípek <jan@zipek.cz>
2025-02-20 08:41:00 +0100

    Improved shop screen styles

--- shop_screen.gd
+++ shop_screen.gd
@@ -8,12 +8,21 @@ signal menu_requested
 @export var skin_items_container: Container
 @export var skin_item_prefab: PackedScene
 @export var back_button: Button
+@export var purchase_confirm_container: Control
+@export var purchase_confirm: Button
+@export var purchase_cancel: Button
+@export var purchase_confirm_label: Label
+
+var current_purchase: SkinData
 
 func _ready():
     refresh()
 
     back_button.pressed.connect(func(): menu_requested.emit())
     visibility_changed.connect(func(): refresh())
+
+    purchase_confirm.pressed.connect(confirm_purchase)
+    purchase_cancel.pressed.connect(func(): purchase_confirm_container.hide())
 
 func refresh():
     var user_data = DataStore.user_data
@@ -25,7 +34,12 @@ func refresh():
 
     for skin_data in game_data.skins:
         var skin_item = skin_item_prefab.instantiate() as SkinShopItem
-        skin_item.set_data(skin_data, user_data.owned_skins.has(skin_data.id), user_data.current_skin == skin_data.id)
+
+        var is_selected = user_data.current_skin == skin_data.id
+        var is_owned = user_data.owned_skins.has(skin_data.id)
+        var is_affordable = user_data.money >= skin_data.cost
+
+        skin_item.set_data(skin_data, is_owned, is_selected, is_affordable)
         skin_items_container.add_child(skin_item)
         skin_item.skin_pressed.connect(buy_skin)
 
@@ -40,10 +54,22 @@ func buy_skin(skin_data: SkinData):
         return
 
     if user_data.money >= skin_data.cost:
-        user_data.money -= skin_data.cost
-        user_data.owned_skins.append(skin_data.id)
-        user_data.current_skin = skin_data.id
+        purchase_confirm_label.text = "Do you want to buy " + skin_data.name + " skin for " + str(skin_data.cost) + " coins?"
+        purchase_confirm_container.show()
+        current_purchase = skin_data
 
-        DataStore.save_user_data()
+func confirm_purchase():
+    if not current_purchase:
+        return
 
-        refresh()
+    var user_data = DataStore.user_data
+
+    user_data.money -= current_purchase.cost
+    user_data.owned_skins.append(current_purchase.id)
+    user_data.current_skin = current_purchase.id
+
+    DataStore.save_user_data()
+
+    refresh()
+
+    purchase_confirm_container.hide()
--- skin_shop_item.gd
+++ skin_shop_item.gd
@@ -7,21 +7,30 @@ signal skin_pressed(skin_data: SkinData)
 @export var cost_label: Label
 @export var icon_texture: TextureRect
 @export var selected_style: StyleBox
+@export var owned_style: StyleBox
+@export var unaffordable_style: StyleBox
+@export var affordable_style: StyleBox
 
 var skin_data: SkinData
 
 func _ready():
     pressed.connect(func(): skin_pressed.emit(skin_data))
 
-func set_data(data: SkinData, owned: bool, _selected: bool):
+func set_data(data: SkinData, owned: bool, selected: bool, affordable: bool):
     skin_data = data
     icon_texture.texture = data.icon
     name_label.text = data.name
     cost_label.text = str(data.cost) if !owned else "Owned"
 
-    if _selected:
+    if selected:
         add_theme_stylebox_override("normal", selected_style)
         add_theme_stylebox_override("hover", selected_style)
+    elif owned:
+        add_theme_stylebox_override("normal", owned_style)
+        add_theme_stylebox_override("hover", owned_style)
+    elif affordable:
+        add_theme_stylebox_override("normal", affordable_style)
+        add_theme_stylebox_override("hover", affordable_style)
     else:
-        remove_theme_stylebox_override("normal")
-        remove_theme_stylebox_override("hover")
+        add_theme_stylebox_override("normal", unaffordable_style)
+        add_theme_stylebox_override("hover", unaffordable_style)
