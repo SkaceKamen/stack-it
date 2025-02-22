df13a4ad4e19967f15a6d250ac36bfabacb2d9f3
Jan Zípek <jan@zipek.cz>
2025-02-22 22:09:28 +0100

    Proper project structure

---
+++ resources/skin_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name SkinData
+
+@export var id: String = ""
+@export var name: String = ""
+@export var cost: int = 0
+@export var icon: Texture2D
+@export var prefab: PackedScene
